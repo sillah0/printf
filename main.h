ba9802d2aa42bffff35f68776a1b0906c9f408c4
IanoNjuguna <inc3099@gmail.com>
2023-10-19 23:47:16 +0300

    main.h: update with header files and struct

--- main.h
+++ main.h
@@ -2,6 +2,21 @@
 #define MAIN_H
 
 #include <stdarg.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ * struct function_z - structure holding an identifier and a function
+ * @identifier: character identifying an action
+ * @print_function: function executed once the identifier is identified
+ */
+typedef struct function_z
+{
+	char *identifier;
+	int (*print_function)(va_list args);
+} function_z;
+
 
 int _putchar(char c);
 int _printf(const char *format, ...);
