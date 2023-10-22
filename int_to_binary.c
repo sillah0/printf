f59b2172b5c00298a19e73fa5891358e25bab9cc
IanoNjuguna <inc3099@gmail.com>
2023-10-23 00:24:50 +0300

    int_to_binary.c: turns unsigned int to binary

---
+++ int_to_binary.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * int_to_binary - turns unsined int to binary
+ * @num: unsigned int
+ *
+ * Return: 0 or 1
+ */
+
+int int_to_binary(int num)
+{
+	unsigned int count = 0;
+
+	if (num / 2)
+		count += int_to_binary(num / 2);
+
+	count += _putchar((num % 2) + '0');
+
+	return (count);
+}
+
